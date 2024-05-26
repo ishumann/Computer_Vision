41b89b9cf7a218a34e763456a15bfcd687495bac
Ishuman Agarwal <mann.agrawal17@gmail.com>
2024-05-26 15:11:51 +0530

    Refactor image_gray_scale.py and crop_image.py

--- opencv_basic/warp_perspective/warp_perspective.py
+++ opencv_basic/warp_perspective/warp_perspective.py
@@ -4,23 +4,22 @@ import numpy as np
 image = cv2.imread("./opencv_basic/Images/cards2.jpg")
 
 
+
 width, height = 500, 500
-
-#pts1=np.float32([[111, 219], [287, 188], [154, 482], [352, 440]])
-pts1=np.float32([[702, 150], [1129, 417], [286, 694], [720, 996]])
-pts2 = np.float32([[0,0], [width, 0], [0, height], [width, height]])
-
 
 cv2.circle(image, (702, 150), 30, (0, 255, 255), 2)
 cv2.circle(image, (1129, 417), 30, (0, 255, 255), 2)
 cv2.circle(image, (286, 694), 30, (0, 255, 255), 2)
 cv2.circle(image, (720, 996), 30, (0, 255, 255), 2)
 
-
 cv2.circle(image, (0,0), 20, (255, 0, 255), -1)
 cv2.circle(image, (width,0), 20, (255, 0, 255), -1)
 cv2.circle(image, (0,height), 20, (255, 0, 255), -1)
 cv2.circle(image, (width,height), 20, (255, 0, 255), -1)
+
+# pts1=np.float32([[111, 219], [287, 188], [154, 482], [352, 440]])
+pts1=np.float32([[702, 150], [1129, 417], [286, 694], [720, 996]])
+pts2 = np.float32([[0,0], [width, 0], [0, height], [width, height]])
 
 
 
