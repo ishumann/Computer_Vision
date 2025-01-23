a89adf985505907d27888db4beb4dad0b593e6f1
ishumann <mann.agrawal17@gmail.com>
2025-01-23 17:03:49 +0530

    feat: Add object detection tracking script using YOLOv8 and improve video output frame rate

--- YOLO_basic/Object_detection_tracking.py
+++ YOLO_basic/Object_detection_tracking.py
@@ -2,85 +2,46 @@ import cv2
 from ultralytics import YOLO
 import math
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('./Videos/video1.mp4')
 model = YOLO("yolov8n.pt")
-coco_classes = ['person','bicycle','car','motorcycle','airplane','bus','train','truck','boat',
-'traffic light',
-'fire hydrant',
-'stop sign',
-'parking meter',
-'bench',
-'bird',
-'cat',
-'dog',
-'horse',
-'sheep',
-'cow',
-'elephant',
-'bear',
-'zebra',
-'giraffe',
-'backpack',
-'umbrella',
-'handbag',
-'tie',
-'suitcase',
-'frisbee',
-'skis',
-'snowboard',
-'sports ball',
-'kite',
-'baseball bat',
-'baseball glove',
-'skateboard',
-'surfboard',
-'tennis racket',
-'bottle',
-'wine glass',
-'cup',
-'fork',
-'knife',
-'spoon',
-'bowl',
-'banana',
-'apple',
-'sandwich',
-'orange',
-'broccoli',
-'carrot',
-'hot dog',
-'pizza',
-'donut',
-'cake',
-'chair',
-'couch',
-'potted plant',
-'bed',
-'dining table',
-'toilet',
-'tv',
-'laptop',
-'mouse',
-'remote',
-'keyboard',
-'cell phone',
-'microwave',
-'oven',
-'toaster',
-'sink',
-'refrigerator',
-'book',
-'clock',
-'vase',
-'scissors',
-'teddy bear',
-'hair drier',
-'toothbrush']
+coco_classes = [
+    "person",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "airplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis"]
+center_points = []
 
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 output = cv2.VideoWriter(
-    "output.avi", cv2.VideoWriter_fourcc(*"XVID"), 60, (frame_width, frame_height)
+    "output.avi", cv2.VideoWriter_fourcc(*"XVID"), 23.98, (frame_width, frame_height)
 )
 
 while cap.isOpened():
@@ -98,6 +59,9 @@ while cap.isOpened():
         x1, y1, x2, y2 = box.xyxy[0]
         print(x1, y1, x2, y2)
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        cx = int((x1+x2)/2)
+        cy = int((y1+y2)/2)
+        center_points.append((cx,cy))
         cv2.rectangle(frame,(x1,y1),(x2,y2), (0,255,0),3)
         conf = math.ceil((box.conf[0]*100))/100
         cls = int(box.cls[0])
@@ -107,6 +71,8 @@ while cap.isOpened():
         c2 = x1+text_size[0],y1-text_size[1]-3
         cv2.rectangle(frame, (x1, y1), c2, [255,0,0], -1, cv2.LINE_AA)
         cv2.putText(frame, label, (x1, y1-2), 0,1,[255,255,255], thickness=1, lineType=cv2.LINE_AA)
+        for pt in center_points:
+            cv2.circle(frame, pt, 2, (0,255,0), -1)
 
     resize_frame = cv2.resize(
         frame, (0, 0), fx=0.6, fy=0.6, interpolation=cv2.INTER_AREA
